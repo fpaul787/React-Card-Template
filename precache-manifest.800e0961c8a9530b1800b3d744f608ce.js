4f91f22ad3bd565f7264ff30cb87370f1a9f1564
Frantz Paul <fpaul787@gmail.com>
2020-07-01 12:34:02 -0400

    Updates

---
+++ precache-manifest.800e0961c8a9530b1800b3d744f608ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01501d659aaca52205d95ad898553949",
+    "url": "/React-Card-Template/index.html"
+  },
+  {
+    "revision": "9c8458db38b6af12f3d9",
+    "url": "/React-Card-Template/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "92d1566d1b63edbfa334",
+    "url": "/React-Card-Template/static/css/main.b3e39221.chunk.css"
+  },
+  {
+    "revision": "9c8458db38b6af12f3d9",
+    "url": "/React-Card-Template/static/js/2.c53b7278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Card-Template/static/js/2.c53b7278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d1566d1b63edbfa334",
+    "url": "/React-Card-Template/static/js/main.5d172859.chunk.js"
+  },
+  {
+    "revision": "a767aa67d332046f53b5",
+    "url": "/React-Card-Template/static/js/runtime-main.21b07ea5.js"
+  },
+  {
+    "revision": "11e20f216e20601300176359120a142e",
+    "url": "/React-Card-Template/static/media/Capital_Punishment_1998.11e20f21.jpg"
+  },
+  {
+    "revision": "020b107a1d893e1d4fca5e9b1cff6e1f",
+    "url": "/React-Card-Template/static/media/LaurynHillTheMiseducationofLaurynHillalbumcover.020b107a.jpg"
+  },
+  {
+    "revision": "228bbd7648cce170fcc095b45827ba39",
+    "url": "/React-Card-Template/static/media/MosDef&TalibKweliBlackStar.228bbd76.jpg"
+  }
+]);
